b8de6a5eebc7a71e07cc2bcf8ed2c95b6acc1694
Ezra <ezraermy@gmail.com>
2023-04-27 01:50:13 +0300

    Dishes test

--- src/redux/recipe/recipeSlice.js
+++ src/redux/recipe/recipeSlice.js
@@ -1,9 +1,9 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
 
 export const fetchRecipe = createAsyncThunk('recipe/fetchRecipe', async (id) => {
-  const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
-  return response.data.meals[0];
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+  const data = await response.json();
+  return data.meals[0];
 });
 
 const recipeSlice = createSlice({
